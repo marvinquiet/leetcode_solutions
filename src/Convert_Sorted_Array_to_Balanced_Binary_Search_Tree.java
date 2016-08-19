06d2b9aab5544683a973772984e2424e7a883efb
wjma <wjma@freewheel.tv>
2016-08-19 15:40:40 +0800

    create new file

--- src/Convert_Sorted_Array_to_Balanced_Binary_Search_Tree.java
+++ src/Convert_Sorted_Array_to_Balanced_Binary_Search_Tree.java
@@ -4,6 +4,7 @@
  * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
  */
 public class Convert_Sorted_Array_to_Balanced_Binary_Search_Tree {
+    // 选中点作为根, 二分法
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums == null || nums.length == 0) return null;
         return generateTree(nums, 0, nums.length-1);
---
+++ src/Convert_Sorted_List_to_Balanced_Binary_Search_Tree.java
@@ -0,0 +1,16 @@
+/**
+ * Created by wjma on 8/19/16.
+ *
+ * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+public class Convert_Sorted_List_to_Balanced_Binary_Search_Tree {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+
+
+    }
+
+    private TreeNode generateTree(int low, int high) {
+
+    }
+}
