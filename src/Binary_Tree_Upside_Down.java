02efa36015a21be267c160c2cb5fdf297cacd149
wjma <wjma@freewheel.tv>
2016-08-22 17:46:37 +0800

    31 Binary Tree Upside Down

--- src/Binary_Tree_Upside_Down.java
+++ src/Binary_Tree_Upside_Down.java
@@ -6,20 +6,29 @@
  ￼where the original right nodes turned into left leaf nodes. Return the new root.
 
  Given a binary tree {1,2,3,4,5},
- 1
- / \
- 2   3
+    1
+   / \
+  2   3
  / \
  4   5
  return the root of the binary tree [4,5,2,#,#,3,1].
  4
  / \
  5   2
- / \
- 3   1
+    / \
+   3   1
  */
 public class Binary_Tree_Upside_Down {
     public TreeNode upsideDownBinaryTree(TreeNode root) {
+        return generateNewRoot(root, null);
+    }
 
+    // 需要一个存parent的点!! 即每次需要存下来parent的位置,防止指针丢失即可。
+    private TreeNode generateNewRoot(TreeNode root, TreeNode parent) {
+        if (root == null) return parent;
+        TreeNode newRoot = generateNewRoot(root.left, root);
+        root.left = (parent == null) ? null : parent.right;
+        root.right = parent;
+        return newRoot;
     }
 }
